7ab1587fc95994ee2e951a3102d07ab759ee0b8d
Yousef M. Y. AlSabbah <PS.Gaming4Ever.NOOB@gmail.com>
2025-06-22 10:27:28 +0300

    feat(auth): centralize auth endpoints in enum and update API calls to use enum values

---
+++ src/features/auth/endpoints.ts
@@ -0,0 +1,9 @@
+export enum AuthEndpoints {
+  Register = '/auth/register/',
+  Activate = '/auth/activate/',
+  TokenCreate = '/auth/token/create/',
+  TokenDestroy = '/auth/token/destroy/',
+  TokenRefresh = '/auth/token/refresh/',
+  TokenVerify = '/auth/token/verify/',
+}
+
